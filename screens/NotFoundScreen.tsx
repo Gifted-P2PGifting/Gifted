e64ce85febf2be7fe4114622c5123ad8a64baf81
mig824 <migmic93@gmail.com>
2020-10-26 22:34:06 -0700

    (update) refactors linting errors

--- screens/NotFoundScreen.tsx
+++ screens/NotFoundScreen.tsx
@@ -4,22 +4,6 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 import { RootStackParamList } from '../types';
 
-export default function NotFoundScreen({
-  navigation,
-}: StackScreenProps<RootStackParamList, 'NotFound'>) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>This screen doesn't exist.</Text>
-      <TouchableOpacity
-        onPress={() => navigation.replace('Root')}
-        style={styles.link}
-      >
-        <Text style={styles.linkText}>Go to home screen!</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -41,3 +25,19 @@ const styles = StyleSheet.create({
     color: '#2e78b7',
   },
 });
+
+export default function NotFoundScreen({
+  navigation,
+}: StackScreenProps<RootStackParamList, 'NotFound'>) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>This screen doesn&apos;t exist.</Text>
+      <TouchableOpacity
+        onPress={() => navigation.replace('Root')}
+        style={styles.link}
+      >
+        <Text style={styles.linkText}>Go to home screen!</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
