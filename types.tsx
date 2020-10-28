c4651f129ed82f49a65ca85c8648d83d9180b73d
Samuel Frakes <samuelafrakes@gmail.com>
2020-10-28 10:30:22 -0700

    type collections updated

--- types.tsx
+++ types.tsx
@@ -4,19 +4,18 @@ export type RootStackParamList = {
 };
 
 export type BottomTabParamList = {
-  TabOne: undefined;
-  TabTwo: undefined;
-  CampaignsTab: undefined;
+  Home: undefined;
+  Feed: undefined;
 };
 
-export type TabOneParamList = {
-  TabOneScreen: undefined;
+export type HomeTabParamList = {
+  HomeScreen: undefined;
+  HistoryScreen: undefined;
+  SettingsScreen: undefined;
 };
 
-export type TabTwoParamList = {
-  TabTwoScreen: undefined;
-};
-
-export type TabCampaignsList = {
+export type FeedTabParamList = {
+  FeedScreen: undefined;
   CampaignScreen: undefined;
+  ItemScreen: undefined;
 };
