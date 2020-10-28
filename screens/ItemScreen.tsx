2fc4ec3a7c9a4ec5c00f0d3da6656326b17ce015
Samuel Frakes <samuelafrakes@gmail.com>
2020-10-28 10:31:18 -0700

    new screens added in coordination with basic navigation setup

---
+++ screens/ItemScreen.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export default function CampaignScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#FFFFFF',
+      }}
+    >
+      <Text>Item</Text>
+    </View>
+  );
+}
