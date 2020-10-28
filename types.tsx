d0da27a01a5d766875d4599d545b01585cc4ae82
mig824 <migmic93@gmail.com>
2020-10-28 12:51:55 -0700

    (add) type for loginParams

--- types.tsx
+++ types.tsx
@@ -4,6 +4,7 @@ export type RootStackParamList = {
 };
 
 export type BottomTabParamList = {
+  LoginScreen: undefined;
   TabOne: undefined;
   TabTwo: undefined;
 };
@@ -14,4 +15,8 @@ export type TabOneParamList = {
 
 export type TabTwoParamList = {
   TabTwoScreen: undefined;
+};
+
+export type LoginScreenParamList = {
+  LoginScreen: undefined;
 };
